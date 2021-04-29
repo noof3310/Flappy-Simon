268cfea18f9d00f4018286a281a5ff7ca043b498
noof3310 <noof3310@gmail.com>
2021-04-29 15:41:38 +0700

    Fix Canvas & Add KT Studio

--- Assets/Scripts/PlayerObstacle.cs
+++ Assets/Scripts/PlayerObstacle.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class PlayerObstacle : MonoBehaviour
 {
-    GameManager gameManager = default;
     void OnCollisionEnter2D(Collision2D collision)
     {
         GameManager.Instance.StopGame();
--- Assets/Scripts/ScoreObstacle.cs
+++ Assets/Scripts/ScoreObstacle.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class ScoreObstacle : MonoBehaviour
 {
-    GameManager gameManager = default;
     void OnTriggerEnter2D(Collider2D collision)
     {
         GameManager.Instance.AddOnePoint();
